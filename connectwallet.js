a2f84f9d274961f8714984ccdc4fc00d5dd50616
Anishn21 <krishnaanish21@gmail.com>
2023-07-15 09:51:59 -0700

    added wallet btn

--- connectwallet.js
+++ connectwallet.js
@@ -1,16 +1,50 @@
 /* To connect using MetaMask */
+let walletAddress = "empty";
+
 async function connect() {
+   if(walletAddress != "empty"){
+      Swal.fire({
+         position: 'top-end',
+         icon: 'success',
+         title: 'Wallet connected already !',
+         showConfirmButton: false,
+         timer: 1500
+      })
+   }
+   else{
+   
     if (window.ethereum) {
        await window.ethereum.request({ method: "eth_requestAccounts" });
        window.web3 = new Web3(window.ethereum);
        const account = web3.eth.accounts;
        //Get the current MetaMask selected/active wallet
-       const walletAddress = account.givenProvider.selectedAddress;
+      walletAddress = account.givenProvider.selectedAddress;
+       Swal.fire({
+         position: 'top-end',
+         icon: 'success',
+         title: 'Your wallet has been connected !',
+         showConfirmButton: false,
+         timer: 1500
+       })
+       
        let btn = document.getElementById("wallet-btn");
-       btn.value=walletAddress;
+       let displayLength = 10;
+       if (walletAddress.length > displayLength) {
+         var displayContent = walletAddress.substring(0, displayLength) + "...";
+         btn.innerHTML = displayContent;
+       } else {
+         btn.innerHTML=walletAddress;
+       }
+       
        console.log(`Wallet: ${walletAddress}`);
     
     } else {
-     console.log("No wallet");
-    }
+     Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'No wallet!'
+    })
+    } 
   }
+  
+}
